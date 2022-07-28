7bae37cb29123929ae1f13c23578febf22387f23
David Lindberg <david.wesley.lindberg@gmail.com>
2022-07-28 13:39:19 -0500

    create simple for loop to console.log 1 to 100

--- eloquent.js
+++ eloquent.js
@@ -28,4 +28,6 @@ print "Buzz" instead.
 
 When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible
 by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those). */
-
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
