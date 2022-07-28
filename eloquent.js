eaf723e7c3f472745fa2e726a5e1e998d24c0791
David Lindberg <david.wesley.lindberg@gmail.com>
2022-07-28 13:52:30 -0500

    add condition to log FizzBuzz for numbers divisible by 3 and 5

--- eloquent.js
+++ eloquent.js
@@ -30,12 +30,13 @@ When you have that working, modify your program to print "FizzBuzz" for numbers 
 by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those). */
 for (let i = 1; i <= 100; i++) {
     // console.log(i);
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
         console.log("Fizz");
     } else if(i % 5 === 0) {
         console.log("Buzz");
-    }
-    else {
+    } else {
         console.log(i);
     }
 }
