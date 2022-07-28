a6ba5609fc7e965d900d0ff8ec432551254069a2
David Lindberg <david.wesley.lindberg@gmail.com>
2022-07-28 13:29:18 -0500

    solve exercise 1

--- eloquent.js
+++ eloquent.js
@@ -1,3 +1,22 @@
 // Chapter 2 - Program Structure
 
-// Exercise - Looping a Trianle
+// Exercise - Looping a Triangle
+/* Write a loop that makes seven calls to console.log to output the following triangle:
+#
+##
+###
+####
+#####
+######
+####### 
+*/
+let triangleString;
+for (let num = 0; num < 7; num += 1) { 
+    if (num === 0) {
+        triangleString = "#";
+        console.log(triangleString);
+    } else {
+        triangleString += "#";
+        console.log(triangleString);
+    }
+}
