38d3a26123daec99363f74eebe12a16f4cbfaad3
David Lindberg <david.wesley.lindberg@gmail.com>
2022-07-28 13:48:21 -0500

    add condition to log Buzz for numbers divisible by 5

--- eloquent.js
+++ eloquent.js
@@ -32,7 +32,10 @@ for (let i = 1; i <= 100; i++) {
     // console.log(i);
     if (i % 3 === 0) {
         console.log("Fizz");
-    } else {
+    } else if(i % 5 === 0) {
+        console.log("Buzz");
+    }
+    else {
         console.log(i);
     }
 }
